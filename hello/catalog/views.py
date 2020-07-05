e8c10bc5bf0bd0f1dba8b6330f07cc340065a7e3
solouyv <solouyv@rambler.ru>
2020-07-05 13:53:43 +0300

    добавлены страницы и взаимосвязь между нини, контроль посещения домашней страницы

--- hello/catalog/views.py
+++ hello/catalog/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Book, Author, BookInstance, Genre
+
+from django.views import generic
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10 
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    # author_book_list = Book.objects.filter(author__iexat = )
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10 
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_books_word = Book.objects.filter(title__icontains = 'first').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres' : num_genres,
+        'num_books_word' : num_books_word,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
