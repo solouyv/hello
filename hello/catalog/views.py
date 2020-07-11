fd1eafb8835780e1773b0ea9649427a99a43113f
solouyv <solouyv@rambler.ru>
2020-07-11 14:52:37 +0300

    выполнена полностью 8 глава с испытанием

--- hello/catalog/views.py
+++ hello/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 class BookDetailView(generic.DetailView):
     model = Book
@@ -12,7 +13,6 @@ class BookListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
-    # author_book_list = Book.objects.filter(author__iexat = )
 
 class AuthorListView(generic.ListView):
     model = Author
@@ -54,7 +54,16 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
     template_name ='catalog/bookinstance_list_borrowed_user.html'
-    paginate_by = 2 
+    paginate_by = 10 
     
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class AllBorrowedBooks(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'catalog.can_mark_returned'
+    model = BookInstance
+    paginate_by = 10
+    template_name = 'catalog/all_bookinstance_list_borrowed.html'
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
